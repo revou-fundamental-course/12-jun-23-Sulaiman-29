e247108e92c579d19c9ac0df8586268c6e33c51e
Sulaiman Azhari <arisatu2nya@gmail.com>
2023-06-22 21:28:48 +0700

    perbaikan

--- js/script.js
+++ js/script.js
@@ -1,34 +1,15 @@
 const numberRegex = /^[0-9]+$/;
 const inputField = document.getElementById("inputField")
+const result = document.getElementById("result")
 
 inputField.addEventListener('keypress', function(event){
     if (!numberRegex.test(event.key)) {
         console.log(event.key)
         event.preventDefault()
-    } 
+    } else {
+        result.textContent = event.key
+        result.style.display = 'inline'
+    }
 })
 
-
-document.getElementById('btn1').onclick = tempConvert;
-document.getElementById('btn2').onclick = clearFrom;
-
-function tempConvert() {
-    var fahrenheit = document.getElementById("Fahrenheit").value;
-    var celcius = document.getElementById("Celcius").value;
-    
-    if (fahrenheit != '') {
-        celcius = (parseFloat(fahrenheit) -32) /1.8; 
-    } else {
-        fahrenheit = (parseFloat(celcius) * 1.8) + 32;
-    }
-
-    document.getElementById('Fahreinheit').value = parseFloat(fahrenheit);
-    document.getElementById('Celcius').value = parseFloat(celcius);
-
-}
-
-function clearFrom() {
-    document.getElementById('Fahrenheit').value = '';
-    document.getElementById('Celcius').value = '';
-}
 
